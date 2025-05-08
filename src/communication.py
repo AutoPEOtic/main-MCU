728c8691e2b8272061becaa17cc4a2a2f4fb38ae
Kristaps <kristap.grava@gmail.com>
2025-05-08 18:49:35 +0300

    initial commit

---
+++ src/communication.py
@@ -0,0 +1,128 @@
+# in this file are defined classes for communication
+
+import time
+import serial
+from pymodbus.client import ModbusSerialClient
+
+#defines a parrent class for general communication with devices
+class communication:
+    def __init__(self, name, port, baudrate):
+        self.name = name
+        self.port = port
+        self.baudrate = baudrate
+        self.serial = serial.Serial(self.port, self.baudrate, timeout=1)
+
+    def sendInstruction():
+        raise NotImplementedError
+    
+    def receiveData():
+        raise NotImplementedError
+
+#TODO test progress: PASSED
+#defines a children class for communication with the main Arduino using serial
+class mainCommunication(communication):
+    def __init__(self, name, port, baudrate):
+        super().__init__(name, port, baudrate)
+        self.serial = serial.Serial(self.port, self.baudrate, timeout=1)
+
+    def sendInstruction(self, instruction):
+        self.serial.write((f'{instruction}\n').encode())
+        print(f'Sending: {instruction.strip()}')
+
+#TODO test progress: PASSED
+#defines a children class for communication with the stepper Arduino using serial
+class stepperCommunication(communication):
+    def __init__(self, name, port, baudrate):
+        super().__init__(name, port, baudrate)
+        self.serial = serial.Serial(self.port, self.baudrate, timeout=1)
+        self.__grblInit()
+
+    def sendInstruction(self, instruction):
+        self.serial.write((f'{instruction}\n').encode())
+        print(f'Sending: {instruction.strip()}')
+
+    def __grblInit(self):
+	    #for debugging prints grbl settings
+        self.serial.write(b'$$\n')
+        time.sleep(1)
+        response = self.serial.readline().decode().strip()
+        print(f'Settings: {response}')
+            
+        #unlocks grbl
+        self.serial.write(b'$X\n')
+        time.sleep(1)
+        response = self.serial.readline().decode().strip()
+        print(f'Unlock response: {response}')
+
+#TODO test progress: PASSED
+#defines a children class for communication with spectroscope's raspberry pico using UART
+class spectromterCommunication(communication):
+    def __init__(self, name, port, baudrate):
+        super().__init__(name, port, baudrate)
+        self.serial = serial.Serial(self.port, self.baudrate)
+
+    def getSpectrum(self):
+        #PUROPOSE: to receive an array that contains spectrum data
+        #WORKFLOW:  1) an activation command is sent to the pico
+        #           2) the pico recieves the byte and executes readSpectrum() function
+        #           3) in the meanwhile getSpectrum() function waits for response
+        #           4) when finished measuring, pico sends spectrum data in the form of list
+        #           5) getSpectrum() command receives answer
+
+        self.serial.write(b'a')
+        spectrum = self.serial.readline().decode().strip()  #in this response is spectrum list
+
+        print(f'Response: {spectrum}')
+        return spectrum
+
+#TODO test progress: NOT PERFORMED
+#defines a children class for communication with PEO using modbus
+class peoCommunication(communication):
+    def __init__(self, name, port, baudrate, parity, stopbits, bytesize, Upos, Ipos, Uneg, Ineg, Pulsepos, Pause1, Pulseneg, Pause2, Multiplier):
+        super().__init__(self, name, port, baudrate)
+        self.parity = parity
+        self.stopbits = stopbits
+        self.bytesize = bytesize
+        self.Upos = Upos
+        self.Ipos = Ipos
+        self.Uneg = Uneg
+        self.Ineg = Ineg
+        self.Pulsepos = Pulsepos
+        self.Pause1 = Pause1
+        self.Pulseneg = Pulseneg
+        self.Pause2 = Pause2
+        self.Multiplier = Multiplier
+
+        self.serial = ModbusSerialClient(self.port, self.baudrate, self.parity, self.stopbits, self.bytesize)
+        self.serial.connect()
+
+    def sendInstruction(self, instruction):
+        #VARIABLES: what do they mean?
+        #   Upos
+        #   Ipos
+        #   Uneg
+        #   Ineg
+        #   Pulsepos
+        #   Pause1
+        #   Pulseneg
+        #   Pause2
+        #   Multiplier
+        #
+        #COMMENT: for PEO datasheet go to peoDatasheet.txt; for original code go to peo.py
+        
+        #writing the values to the registers
+        self.serial.write_register(address=0, value=self.Upos, slave=20)
+        self.serial.write_register(address=1, value=self.Ipos, slave=20)
+        self.serial.write_register(address=2, value=self.Uneg, slave=20)
+        self.serial.write_register(address=3, value=self.Ineg, slave=20)
+        self.serial.write_register(address=4, value=self.Pulsepos, slave=20)
+        self.serial.write_register(address=5, value=self.Pause1, slave=20)
+        self.serial.write_register(address=6, value=self.Pulseneg, slave=20)
+        self.serial.write_register(address=7, value=self.Pause2, slave=20)
+        self.serial.write_register(address=8, value=self.Multiplier, slave=20)
+        
+        #forcing coils to update the display
+        serial.write_coil(2, True, slave=20)
+        serial.write_coil(3, True, slave=20)
+
+        time.sleep(1)
---
+++ src/config.py
@@ -0,0 +1,27 @@
+#[main arduino]
+peripheral_pico_port = "/dev/ttyACM0"
+peripheral_pico_baudrate = 115200
+
+#[stepper arduino]
+stepper_port = "/dev/ttyUSB1"
+stepper_baudrate = 115200
+
+#[spectroscope]
+spectroscope_port = "/dev/ttyACM1"
+baudrate = 9600
+
+#[PEO]
+PEO_port = "/dev/ttyUSB2"
+PEO_baudrate = 19200
+PEO_parity = "E"
+PEO_stopbits = 1
+PEO_bytesize = 8
+PEO_Upos = 300
+PEO_Ipos = 100
+PEO_Uneg = 10
+PEO_Ineg = 100
+PEO_Pulsepos = 100
+PEO_Pause1 = 100
+PEO_Pulseneg = 100
+PEO_Pause2 = 100
+PEO_Multiplier = 3
